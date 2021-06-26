358ba9d7f5c76329de077a94f3fd6f9017dcdbc7
Roman Svechnikov <rsvechnikov@ya.ru>
2021-06-26 12:02:07 +0300

    choose out folder with a script parameter and use more informative out file name

--- demo.sh
+++ demo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 WAV_FILE=$1
+OUT_DIR=${2:-result}
+OUT_NAME=$(basename "$WAV_FILE" .wav)
 FEAT_DIR="feat"
 NOTE_DIR="note"
-OUT_DIR="result"
 MODEL_DIR="model"
 
 START=15
@@ -21,7 +22,7 @@ python3 NoteSeg.py -d ${FEAT_DIR}/demo.feat -p ${FEAT_DIR}/demo.pitch -of ${NOTE
                    -m ${MODEL_DIR}/resnet18_80.model --feat 9 --threshold 0.5
 
 # Transcription to Results
-python3 Est2Midi.py ${NOTE_DIR}/demo.est ${OUT_DIR}/demo.midi
+python3 Est2Midi.py ${NOTE_DIR}/demo.est ${OUT_DIR}/${OUT_NAME}.midi
 
 # Visualization
 # python3 Visualize.py -pitch ${FEAT_DIR}/demo.pitch -est ${NOTE_DIR}/demo.est -z ${FEAT_DIR}/demo.z \
